6bfe1a0b1941f463b0211cdbbf8b22d2bf79aecd
Luc Girardin <luc.girardin@macrofocus.com>
2019-01-18 22:50:58 +0100

    Integrate Blueprint

---
+++ dist/react/src/index-react-blueprint.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Blueprint from "@blueprintjs/core";
+import * as BlueprintSelect from "@blueprintjs/select";
+import * as BlueprintTable from "@blueprintjs/table";
+
+//If you don't need rendering to strings comment out the following line
+import ReactDOMServer from "react-dom/server";
+
+//Export the base React packages to the Window object for GWT to access
+window.React = React;
+window.ReactDOM = ReactDOM;
+
+//If you don't need rendering to strings comment out the following line
+window.ReactDOMServer = ReactDOMServer;
+
+window.Blueprint = Blueprint;
+
+// window.Button = {
+//     Button : Button
+// }
+
