24bd50d03d0bca68726c86569964afad48155cc0
Luc Girardin <luc.girardin@macrofocus.com>
2019-04-26 08:54:08 +0200

    Integrate Blueprint

--- dist/react/src/index-react-blueprint.js
+++ dist/react/src/index-react-blueprint.js
@@ -2,9 +2,20 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+
 import * as Blueprint from "@blueprintjs/core";
 import * as BlueprintSelect from "@blueprintjs/select";
 import * as BlueprintTable from "@blueprintjs/table";
+
+// import Blueprint from "@blueprintjs/core";
+// import {Button,Collapse,Icon,Menu,ResizeSensor,Slider,Switch,Text,Tree,FormGroup,Checkbox,MenuItem,NumericInput} from "@blueprintjs/core";
+// import BlueprintSelect from "@blueprintjs/select";
+// import {Select} from "@blueprintjs/select";
+// import BlueprintTable from "@blueprintjs/table";
+// import {Table,Column} from "@blueprintjs/table";
+
+// import "@blueprintjs/core/lib/css/blueprint.css"
+// import "@blueprintjs/table/lib/css/table.css";
 
 //If you don't need rendering to strings comment out the following line
 import ReactDOMServer from "react-dom/server";
@@ -17,6 +28,28 @@ window.ReactDOM = ReactDOM;
 window.ReactDOMServer = ReactDOMServer;
 
 window.Blueprint = Blueprint;
+window.BlueprintSelect = BlueprintSelect;
+window.BlueprintTable = BlueprintTable;
+
+// window.Blueprint = Blueprint;
+// window.Blueprint.Button = Button;
+// window.Blueprint.Collapse = Collapse;
+// window.Blueprint.Icon = Icon;
+// window.Blueprint.Menu = Menu;
+// window.Blueprint.ResizeSensor = ResizeSensor;
+// window.Blueprint.Slider = Slider;
+// window.Blueprint.Switch = Switch;
+// window.Blueprint.Text = Text;
+// window.Blueprint.Tree = Tree;
+// window.Blueprint.FormGroup = FormGroup;
+// window.Blueprint.Checkbox = Checkbox;
+// window.Blueprint.MenuItem = MenuItem;
+// window.Blueprint.NumericInput = NumericInput;
+// window.BlueprintSelect = BlueprintSelect;
+// window.BlueprintSelect.Select = Select;
+// window.BlueprintTable = BlueprintTable;
+// window.BlueprintTable.Table = Table;
+// window.BlueprintTable.Column = Column;
 
 // window.Button = {
 //     Button : Button
